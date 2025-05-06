f48880a87e2e57226a211212f5d3f168af3dd0c4
vivid-storage <165187161+vivid-storage@users.noreply.github.com>
2025-05-06 12:38:31 -0700

    Create __init__.py

---
+++ dummy_pkg/__init__.py
@@ -0,0 +1,11 @@
+"""
+Dummy package for testing purposes.
+This package doesn't do anything useful.
+"""
+
+__version__ = "0.1.0"
+
+from .core import hello_world
+
+# Just provide a simple welcome message
+print("Dummy package imported successfully!")
